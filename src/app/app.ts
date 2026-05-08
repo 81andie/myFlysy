dd640829d48f29c2e6a948e984cb0df283034d52
81andie <133395577+81andie@users.noreply.github.com>
2026-05-08 22:47:28 +0200

    iniciando el proyecto

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,19 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal,OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { initFlowbite } from 'flowbite';
+import { Navbar } from "./components/navbar/navbar";
+import { Sidenav } from "./components/sidenav/sidenav";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, Sidenav],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('myFlysy');
+
+   ngOnInit(): void{
+    initFlowbite()
+  }
 }
