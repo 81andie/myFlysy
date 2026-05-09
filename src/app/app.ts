7d210d45b2bdc348438bbeacc0b285275b569f12
81andie <133395577+81andie@users.noreply.github.com>
2026-05-09 15:24:19 +0200

    añadiendo el mapa, i ñadiendo el logo

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { initFlowbite } from 'flowbite';
 import { Navbar } from "./components/navbar/navbar";
 import { Sidenav } from "./components/sidenav/sidenav";
+import {  MapComponent } from "./components/map/map";
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Sidenav],
+  imports: [RouterOutlet, Navbar, Sidenav,MapComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/components/map/map.ts
+++ src/app/components/map/map.ts
@@ -1,4 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Map from 'ol/Map.js';
+import { defaults as defaultControls } from 'ol/control/defaults.js';
+import DragRotateAndZoom from 'ol/interaction/DragRotateAndZoom.js';
+import {defaults as defaultInteractions} from 'ol/interaction/defaults.js';
+import FullScreen from 'ol/control/FullScreen.js';
+import TileLayer from 'ol/layer/Tile.js';
+import TileJSON from 'ol/source/TileJSON.js';
+import Style from 'ol/style/Style';
+import Attribution from 'ol/control/Attribution.js';
+import View from 'ol/View.js';
+import Layer from 'ol/layer/WebGLTile.js';
+import Source from 'ol/source/ImageTile.js';
+import { fromLonLat } from 'ol/proj.js';
+import XYZ from 'ol/source/XYZ';
+
 
 @Component({
   selector: 'app-map',
@@ -6,6 +21,39 @@ import { Component } from '@angular/core';
   templateUrl: './map.html',
   styleUrl: './map.css',
 })
-export class Map {
+export class MapComponent implements OnInit {
+
+
+  private map: Map | null = null;
+
+
+  ngOnInit(): void {
+
+    this.map = new Map({
+      controls: defaultControls().extend([new FullScreen()]),
+      interactions: defaultInteractions().extend([new DragRotateAndZoom()]),
+      layers: [
+        new TileLayer({
+          source: new XYZ({
+            url:'https://api.maptiler.com/maps/hybrid-v4/{z}/{x}/{y}@2x.jpg?key=bZ943IENWwexU3umotpo',
+            attributions: '© MapTiler'
+          }),
+        }),
+      ],
+      target: 'map',
+      view: new View({
+        center: [311158.68373997946, 5157606.481663526],
+        zoom: 5,
+      }),
+
+
+    });
+
+
+  }
+
+
+
+
 
 }
